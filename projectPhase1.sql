d44065ed64d0cb8b2028c35c6373426ada607ce5
aya-ah2003532 <124076159+aya-ah2003532@users.noreply.github.com>
2023-09-20 16:30:07 +0300

    SQL tables and schema updated V2

--- projectPhase1.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    TABLE_NAME as 'Table Name',
-    COLUMN_NAME as 'Column Name',
-    REFERENCED_TABLE_NAME as 'Referenced Table',
-    REFERENCED_COLUMN_NAME as 'Referenced Column'
-FROM
-    INFORMATION_SCHEMA.KEY_COLUMN_USAGE
-WHERE
-    CONSTRAINT_SCHEMA = 'project';
